09e516f399cf1b6663f771207c7cb7d2354f1aa1
Aiden Harvey <aidenharvey102@gmail.com>
2019-02-11 17:39:44 +0000

    connected app to mongo databse

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task-manager'
+app.config["MONGO_URI"] = 'mongodb://admin:Xb0x3869@ds331735.mlab.com:31735/task-manager'
+
+mongo =PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+# @app.route('/')
+# def hello():
+#     return 'Hello World'
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
-
